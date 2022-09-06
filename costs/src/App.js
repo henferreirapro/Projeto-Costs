1d607beb0b9bb9c1c15e669d4d2c775975b2ada4
Henrique Ferreira <62857389+henferreiraofc@users.noreply.github.com>
2022-09-05 22:04:12 -0300

    Component Container
    1 - Add o Component Container para encapsular as rotas(Routes) e poder controlar outros aspectos desses componentes.
    
    2 - Assim posso add alguns comportamentos com os estilos que add no Container.module.css
    
    3 - dentro da tag Container add um customClass para poder mudar futuramente o comportamento dos itens como aumentar ou diminuir largura, deixar a direção dos elementos em column, flex-start, entre outros

--- costs/src/App.js
+++ costs/src/App.js
@@ -7,16 +7,20 @@ import Contact from './components/pages/Contact'
 import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
 
+import Container from './components/layout/Container'
+
 function App() {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/company" element={<Company />} />
-        <Route exact path="/newproject" element={<NewProject />} />
-        <Route exact path="/contact" element={<Contact />} />
-      </Routes>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
+      </Container>
       <Footer />
     </Router>
   );
